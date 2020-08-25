065719fd7bd466bdfaf711c4f94ad2e2cdf75cd3
Sameer Khalid <54281599+sameerkhalid007@users.noreply.github.com>
2020-08-25 08:45:26 +0530

    Add files via upload

---
+++ Contact_Sam.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+$city = $_REQUEST['city'];
+$message = $_REQUEST['message'];
+
+if (empty($name) || empty($email) || empty($phone) || empty($city) || empty($message)) {
+  echo "<script type='text/javascript'>alert("Please fill in all fields.")</script>";
+} else {
+  // code...
+  mail("khalidsameer117@gmail.com","Sam Contact Form", $message, "From: $name <$email>");
+  echo "<script type='text/javascript'>alert("Message Sent Successfully !");
+    window.history.go(-1);
+    </script>";
+}
+ ?>
