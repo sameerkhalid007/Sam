6ebfc2196506aba183ae4809891cffee27a0961a
Sameer Khalid <54281599+sameerkhalid007@users.noreply.github.com>
2020-08-25 10:04:50 +0530

    Update Contact_Sam.php

--- Contact_Sam.php
+++ Contact_Sam.php
@@ -9,10 +9,7 @@ $message = $_REQUEST['message'];
 if (empty($name) || empty($email) || empty($phone) || empty($city) || empty($message)) {
   echo "<script type='text/javascript'>alert("Please fill in all fields.")</script>";
 } else {
-  // code...
   mail("khalidsameer117@gmail.com","Sam Contact Form", $message, "From: $name <$email>");
-  echo "<script type='text/javascript'>alert("Message Sent Successfully !");
-    window.history.go(-1);
-    </script>";
+  echo "Message Sent Successfully !";
 }
  ?>
